dc665ed5342adc4c19ddcf2bf734683cd5a15080
wkopp <kopp@molgen.mpg.de>
2015-08-05 11:33:20 +0200

    minor changes only

--- src/Rbackground.c
+++ src/Rbackground.c
@@ -42,44 +42,22 @@ void Rmakebg(char **infasta, int *order) {
     error("%s not found!",infasta[0]);
     return;
   }
-
+  Rprintf("makebg: order=%d\n",order[0]);
 
   if (order[0]>0) {
-  #ifdef IN_R
     Rstation=Calloc(power(ALPHABETSIZE,order[0]),double);
     count=Calloc(power(ALPHABETSIZE,order[0]+1),double);
     Rtrans=Calloc(power(ALPHABETSIZE, order[0]+1),double);
-  #else
-    Rstation=calloc(power(ALPHABETSIZE,order[0]),sizeof(double));
-    count=calloc(power(ALPHABETSIZE,order[0]+1),sizeof(double));
-    Rtrans=calloc(power(ALPHABETSIZE, order[0]+1),sizeof(double));
-  #endif
 
     getNucleotideFrequencyFromSequence(f,count, order[0]);
-    #ifdef WK
-    for(i=0;i<power(ALPHABETSIZE, order[0]+1); i++) {
-      Rprintf("c[%d]=%e\n",i,count[i]);
-    }
-    #endif
 
     getForwardTransition(count, Rtrans, order[0]);
-    #ifdef WK
-    for(i=0;i<power(ALPHABETSIZE, order[0]+1); i++) {
-      Rprintf("T[%d]=%e\n",i,Rtrans[i]);
-    }
-    #endif
     getStationaryDistribution(Rtrans, Rstation, order[0]);
 
   } else {
-  #ifdef IN_R
     Rstation=Calloc(power(ALPHABETSIZE,order[0]+1),double);
     Rtrans=Calloc(power(ALPHABETSIZE,order[0]+1),double);
     count=Calloc(power(ALPHABETSIZE,order[0]+1),double);
-  #else
-    Rstation=calloc(power(ALPHABETSIZE,order[0]+1),sizeof(double));
-    Rtrans=calloc(power(ALPHABETSIZE,order[0]+1),sizeof(double));
-    count=calloc(power(ALPHABETSIZE,order[0]+1),sizeof(double));
-  #endif
     getNucleotideFrequencyFromSequence(f,count, order[0]);
     getForwardTransition(count, Rstation, order[0]);
     getForwardTransition(count, Rtrans, order[0]);
@@ -92,11 +70,7 @@ void Rmakebg(char **infasta, int *order) {
     Rprintf("mu[%d]=%e\n",i,Rstation[i]);
   }
   #endif
-  #ifdef IN_R
   Free(count);
-  #else
-  free(count);
-  #endif
 }
 
 void RgetOrder(int *o) {
@@ -114,30 +88,30 @@ void RgetBackground(double *station, double *trans) {
 
 void RprintBackground() {
   int i;
-  int order_;
 
-  if (Rorder>0) order_=Rorder;
-  else order_=1;
-  for (i=0; i<power(ALPHABETSIZE, order_); i++) {
-    Rprintf("mu(i=%d)=%e\n", i, Rstation[i]);
-  }
-  for (i=0; i<power(ALPHABETSIZE, order_+1); i++) {
-    Rprintf("T(i=%d)=%e\n", i, Rtrans[i]);
-  }
+  if (Rorder>0) {
+    for (i=0; i<power(ALPHABETSIZE, Rorder); i++) {
+      Rprintf("mu(i=%d)=%e\n", i, Rstation[i]);
+    }
+    for (i=0; i<power(ALPHABETSIZE, Rorder+1); i++) {
+      Rprintf("T(i=%d)=%e\n", i, Rtrans[i]);
+    }
+	} else {
+
+    for (i=0; i<power(ALPHABETSIZE, 1); i++) {
+      Rprintf("mu(i=%d)=%e\n", i, Rstation[i]);
+    }
+    for (i=0; i<power(ALPHABETSIZE, 1); i++) {
+      Rprintf("T(i=%d)=%e\n", i, Rtrans[i]);
+    }
+	}
 }
 
 void RdestroyBackground() {
 
-#ifdef IN_R
   if(Rstation) Free(Rstation);
   if(Rtrans) Free(Rtrans);
-#else
-  if(Rstation) free(Rstation);
-  if(Rtrans) free(Rtrans);
-#endif
   Rstation=NULL;
   Rtrans=NULL;
-  //RdeleteBeta();
-  //RdeleteDelta();
 }
 
