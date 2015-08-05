483ee8c199e48a435aecbf825759fcc45dbd0863
wkopp <kopp@molgen.mpg.de>
2015-08-05 16:34:15 +0200

    Rename Bayes method to dynamic programming method

--- inst/dynprog_paper/plotdynprog.R
+++ inst/dynprog_paper/plotdynprog.R
@@ -14,13 +14,13 @@ op=overlap.prob()
 cpdist=comp.pois(seqlen, numofseqs, maxhits,maxclumpsize=60, op)
 cpdist.pape=comp.pois.pape(seqlen, numofseqs, maxhits,maxclumpsize=100, op)
 #cpdist.pape=comp.pois.pape(seqlen, numofseqs, maxhits,maxclumpsize=100, op)
-bayes=posterior.count(seqlen, numofseqs, maxhits, op)
+db=dynprog.count(seqlen, numofseqs, maxhits, op)
 
 df=data.frame(hits=1:maxhits -1,
 							prob=mean_s,
 							cpnew=cpdist[[1]][1:maxhits],
 #							cppape=cpdist.pape[[1]][1:maxhits],
-							bayes=bayes[[1]][1:maxhits],
+							db=dynprog[[1]][1:maxhits],
 							bin=dbinom(1:maxhits -1, 2*(seqlen-nrow(pwm)+1)*numofseqs,
 												 op$alpha),
 											q25=q25_s,q75=q75_s)
@@ -37,7 +37,7 @@ pl=ggplot(df,aes(x=hits,y=prob, col=Model))+
   geom_errorbar(aes(ymin=q25,ymax=q75),width=.1,color="black")
   #geom_line()+
   pl=pl+geom_point()
-pl<-pl+geom_point(data=df, aes(x=hits,y=bayes,col="Bayes"), size=3)
+pl<-pl+geom_point(data=df, aes(x=hits,y=db,col="Dyn. prog."), size=3)
 pl<-pl+geom_point(data=df, aes(x=hits,y=cpnew,col="Comp. Poisson"))
 #pl<-pl+geom_point(data=df, aes(x=hits,y=cppape,col="Comp. Poisson (Pape)"))
 pl<-pl+geom_line(data=df, aes(x=hits,y=bin,col="Binomial dist."))
@@ -58,10 +58,10 @@ qsig=which(cumsum(df2$prob)>=0.95)
 r=3
 write(
 paste(paste(alpha,seqlen, 
-signif(sum(abs(df2$prob-df2$bayes)),digits=r),
+signif(sum(abs(df2$prob-df2$db)),digits=r),
 signif(sum(abs(df2$prob-df2$cpnew)), digits=r),
 signif(sum(abs(df2$prob-df2$bin)),digits=r),
-signif(sum(abs(df2$prob[qsig]-df2$bayes[qsig])),digits=r),
+signif(sum(abs(df2$prob[qsig]-df2$db[qsig])),digits=r),
 signif(sum(abs(df2$prob[qsig]-df2$cpnew[qsig])),digits=r),
 signif(sum(abs(df2$prob[qsig]-df2$bin[qsig])),digits=r),sep=" & "), "\\\\"),
 file=sprintf("%s/%s_10-%d_m%d.tabpart", figdir,pwmname,lalpha,markov))
@@ -73,11 +73,11 @@ r=3
 write(
 paste(paste(alpha,seqlen, 
 signif(average(df2$prob),digits=r),
-signif(average(df2$bayes),digits=r),
+signif(average(df2$db),digits=r),
 signif(average(df2$cpnew),digits=r),
 signif(average(df2$bin),digits=r),
 signif(variance(df2$prob),digits=r),
-signif(variance(df2$bayes),digits=r),
+signif(variance(df2$db),digits=r),
 signif(variance(df2$cpnew),digits=r),
 signif(variance(df2$bin),digits=r) ,sep=" & "), "\\\\"),
 file=sprintf("%s/%s_10-%d_m%d.mu_sig.tabpart", figdir,pwmname,lalpha,markov))
