e5e408f6239d7e0398b22ed22253e67dd3088905
wkopp <kopp@molgen.mpg.de>
2015-08-05 11:41:26 +0200

    added read.background.sampling

--- R/background_wrapper.R
+++ R/background_wrapper.R
@@ -1,11 +1,11 @@
 
 #interface functions for motif representation
 read.background=function(file, order=1) {
-  if (regexpr(pattern=".fasta$",file)>=0) {
     dummy=.C("Rmakebg", as.character(file), as.integer(order))
-  } else {
-    dummy=.C("RreloadBackground", as.character(file))
-  }
+  #if (regexpr(pattern=".fasta$",file)>=0) {
+  #} else {
+  #  dummy=.C("RreloadBackground", as.character(file))
+  #}
 }
 
 store.background=function(file) {
@@ -18,4 +18,24 @@ print.background=function() {
 
 delete.background=function() {
   dummy=.C("RdestroyBackground")
+}
+
+read.background.sampling=function(file, order=1) {
+  #if (regexpr(pattern=".fasta$",file)>=0) {
+    dummy=.C("RmakebgForSampling", as.character(file), as.integer(order))
+  #} else {
+  #  dummy=.C("RreloadBackgroundForSampling", as.character(file))
+  #}
+}
+
+store.background.sampling=function(file) {
+  dummy=.C("RstorebgForSampling",as.character(file));
+}
+
+print.background.sampling=function() {
+  dummy=.C("RprintBackgroundForSampling");
+}
+
+delete.background.sampling=function() {
+  dummy=.C("RdestroyBackgroundForSampling")
 }
