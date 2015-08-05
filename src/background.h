0ac78fed75acb073696b124882ef3d525c676156
wkopp <kopp@molgen.mpg.de>
2015-08-05 11:39:00 +0200

    added writeBackground

--- src/background.h
+++ src/background.h
@@ -15,4 +15,5 @@ void readBackground (FILE *f, double **station, double **trans, int *order);
 void deleteBackground(double * station, double *trans);
 int getStationaryDistribution(double *trans, double *station, int order);
 int makebg(char *infasta, char *outmodel, char *outseq, char *order);
+void writeBackground(FILE *f, double * station, double * trans, int order);
 #endif
