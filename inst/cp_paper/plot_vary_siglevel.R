2a40e2b4d20fb7660602050f76c9b635cd4aa122
wkopp <kopp@molgen.mpg.de>
2016-01-06 09:50:17 +0100

    changed plotting parameters

--- inst/cp_paper/plot_vary_siglevel.R
+++ inst/cp_paper/plot_vary_siglevel.R
@@ -2,22 +2,15 @@ library(mdist)
 library(seqLogo)
 library(ggplot2)
 paperdir=Sys.getenv("CPPAPER")
+paperdir="/project/Tf2motif/home/thesis"
 figdir=sprintf("%s/figures", paperdir)
 variants=data.frame(alpha=c(rep(.01,3),rep(0.001,3*3)),
 										seqlen=c(rep(1000,3),rep(10000,3*3)),
 										markov=c(rep(1,3),rep(1,3),rep(0,3),rep(2,3)),
 										name=rep(c("x32","x21","e47"),4),
 										type=rep(c("tab","tab","transfac"),4))
-#alpha=0.001
-#lalpha=-log10(alpha)
 gran=0.1
-#mdist.option(alpha, gran)
-#seqlen=10000
-numofseqs=1
 maxhits=100
-#markov=0
-
-addlegend=FALSE
 
 for (i in 1:nrow(variants)) {
 	alpha=variants$alpha[i]
@@ -28,38 +21,24 @@ for (i in 1:nrow(variants)) {
   pwmname=variants$name[i]
 
   print (sprintf("%s:alpha=%e, slen=%d, markov=%d", pwmname,alpha,seqlen,markov))
-	#alpha=variants$alpha[i]
-# reproduce figure 2a,c,e use seqlen=1000, alpha=0.01, markov=1
-# reproduce figure 2b,d,f use seqlen=10000, alpha=0.001, markov=1
-# reproduce figure 3a,c,e use seqlen=10000, alpha=0.001, markov=0
-# reproduce figure 3b,d,f use seqlen=10000, alpha=0.001, markov=2
 
-seqfile=system.file("extdata","seq.fasta", package="mdist")
-read.background(seqfile,markov)
-read.background.sampling(seqfile,markov)
+  seqfile=system.file("extdata","seq.fasta", package="mdist")
+  read.background(seqfile,markov)
+  read.background.sampling(seqfile,markov)
 
-#pwmname="x32"
-pwmfile=system.file("extdata",paste(pwmname,".pwm",sep="",collapse=""),
- package="mdist")
-#seqfile="dhsclusters/cluster10.bed.fasta"
+  pwmfile=system.file("extdata",paste(pwmname,".pwm",sep="",collapse=""),
+  package="mdist")
 
-#load motif model from file
-read.motif(pwmfile,variants$type[i], 0.01)
-pwm=motif2matrix()
+  read.motif(pwmfile,variants$type[i], 0.01)
+  pwm=motif2matrix()
 
-#postscript(file=sprintf("%s/pal.eps", figdir))
-#seqLogo(pwm)
-#dev.off()
-
-source(system.file("cp_paper","plotpwm.R", package="mdist"))
+  source(system.file("cp_paper","plotpwm.R", package="mdist"))
 }
 
 pwmname="x32"
 pwmfile=system.file("extdata",paste(pwmname,".pwm",sep="",collapse=""),
  package="mdist")
-#seqfile="dhsclusters/cluster10.bed.fasta"
 
-#load motif model from file
 read.motif(pwmfile,"tab", 0.01)
 pwm=motif2matrix()
 
@@ -70,7 +49,6 @@ dev.off()
 pwmname="x21"
 pwmfile=system.file("extdata",paste(pwmname,".pwm",sep="",collapse=""),
  package="mdist")
-#seqfile="dhsclusters/cluster10.bed.fasta"
 
 #load motif model from file
 read.motif(pwmfile,"tab", 0.01)
--- inst/cp_paper/plotpwm.R
+++ inst/cp_paper/plotpwm.R
@@ -1,107 +1,106 @@
 
 Smat=matrix(0,ncol=maxhits,nrow=100)
 for (isim in 1:100) {
-  simc=sim.counts(seqlen,numofseqs,maxhits,nsim=1000)
+  simc=sim.counts(seqlen,maxhits,nsim=1000)
   Smat[isim,]=simc[[1]][1:(maxhits)]
 }
-mean_s=apply(Smat,2,median)
+mean_s=apply(Smat,2,mean)
 q75_s=apply(Smat,2,quantile,.75)
 q25_s=apply(Smat,2,quantile,.25)
-#se_s=apply(Smat,2,sd)/sqrt(10)
 
 
 op=overlap.prob()
-cpdist=comp.pois(seqlen, numofseqs, maxhits,maxclumpsize=60, op)
-cppape=comp.pois.pape(seqlen, numofseqs, maxhits,maxclumpsize=100, op)
+cpdist=comp.pois(seqlen,  maxhits,maxclumpsize=60, op)
+cppape=comp.pois.pape(seqlen, maxhits,maxclumpsize=100, op)
 
-df=data.frame(hits=1:maxhits -1,
-							prob=mean_s,
-							cpnew=cpdist[[1]][1:maxhits],
-							cppape=cppape[[1]][1:maxhits],
-							bin=dbinom(1:maxhits -1, 2*(seqlen-nrow(pwm)+1)*numofseqs,
-												 op$alpha),
-											q25=q25_s,q75=q75_s)
-#pdist=posterior.count(seqlen, numofseqs, maxhits, op)
-#dist=comp.pois(seqlen, numofseqs, maxhits,maxclumpsize=30)
-#p.value=comp.pois.test(num.motifhits(seqfile), maxhit=150)
-#pape=comp.pois(seqlen, numofseqs, maxhits,maxclumpsize=30, method="pape")
-#post=posteriorcount()
-df2=df
-postscript(file=sprintf("%s/%s_10-%d_m%d.eps", figdir,pwmname,lalpha,markov))
-df=subset(df2, hits<60)
-Model="Sampled dist."
-pl=ggplot(df,aes(x=hits,y=prob, col=Model))+
-  geom_errorbar(aes(ymin=q25,ymax=q75),width=.1,color="black")
-  #geom_line()+
-  pl=pl+geom_point()
-pl<-pl+geom_point(data=df, aes(x=hits,y=cpnew,col="Comp. Poisson (new)"), size=3)
-pl<-pl+geom_point(data=df, aes(x=hits,y=cppape,col="Comp. Poisson (Pape)"))
-pl<-pl+geom_line(data=df, aes(x=hits,y=bin,col="Binomial dist."))
-pl<- pl+scale_colour_manual(values=c("gray", "blue", "red", "black"))
-pl=pl + ylab("P(# of hits)") + 
-xlab("# of hits") 
-if (addlegend==TRUE) {
-pl=pl+theme(panel.background=element_rect(fill='white', colour="white"),
-						legend.text=element_text(size=16), axis.text.x=element_text(size=16),
-			axis.text.y=element_text(size=16), text=element_text(size=16))
-} else {
-pl=pl+theme_bw()+ 
- theme(panel.background=element_rect(fill='white', colour="black"), 
-						legend.position="none")
-}
+dft=data.frame(hits=rep(1:maxhits -1,4),
+							prob=c(mean_s,
+							cpdist[[1]][1:maxhits],
+							cppape[[1]][1:maxhits],
+							dbinom(1:maxhits -1, 2*(seqlen-nrow(pwm)+1),
+												 op$alpha)),
+							model=factor(c(rep("emp",maxhits),
+														 rep("cpk",maxhits),
+											 rep("cpp",maxhits),rep("bin",maxhits))),
+							q25=c(q25_s, cpdist[[1]][1:maxhits],
+							             cppape[[1]][1:maxhits],
+							            dbinom(1:maxhits -1, 2*(seqlen-nrow(pwm)+1),
+							     			                          op$alpha)),
+							q75=c(q75_s, cpdist[[1]][1:maxhits],
+							             cppape[[1]][1:maxhits],
+							            dbinom(1:maxhits -1, 2*(seqlen-nrow(pwm)+1),
+							     			                          op$alpha)))
+
+df=subset(dft, hits<60)
+postscript(file=sprintf("%s/%s_10-%d_m%d.eps", figdir,pwmname,lalpha,markov),
+					 width=6,height=4)
+pl=ggplot(df,aes(x=hits,y=prob, col=model, shape=model))+
+  labs(y=expression("P(X)"),x="Num. of hits")+ geom_point()+
+  scale_colour_manual(name="Models",
+  	 limits=c("emp","cpk","cpp","bin"), values=c("black", "blue", "red", "grey"),
+  										labels=c(expression('P'["E"]*"(X)"),
+  														 expression('P'["CP"]^"K"*"(X)"),
+  														 expression('P'["CP"]^"P"*"(X)"),
+  														 expression('P'["Bin"]*"(X)")))+
+  scale_shape_manual(name="Models",
+  	 limits=c("emp","cpk","cpp","bin"), values=c(20,19,17,3),
+  										labels=c(expression('P'["E"]*"(X)"),
+  														 expression('P'["CP"]^"K"*"(X)"),
+  														 expression('P'["CP"]^"P"*"(X)"),
+  														 expression('P'["Bin"]*"(X)")))+
+  geom_errorbar(aes(ymin=q25,ymax=q75),width=.1)+ theme_bw()+
+  theme(text=element_text(size=20),
+  			legend.position=c(0.7,.7),
+  			legend.text=element_text(size=18),
+  			legend.key.size=unit(.4,"inches"))
 print(pl)
 dev.off()
-qsig=which(cumsum(df2$prob)>=0.95)
 
+pk=subset(df,model=="cpk")
+pe=subset(df,model=="emp")
+pp=subset(df,model=="cpp")
+pb=subset(df,model=="bin")
+qsig=which(cumsum(pe$prob)>=0.95)
+
+postscript(file=sprintf("%s/%s_10-%d_m%d_part.eps", figdir,pwmname,lalpha,markov),
+					 width=6,height=4)
+dfp=subset(dft,hits %in% qsig)
+
+plp=ggplot(dfp,aes(x=hits,y=prob, col=model, shape=model))+
+  labs(y=expression("P(X)"),x="Num. of hits")+ geom_point()+
+  scale_colour_manual(name="Models",
+  	 limits=c("emp","cpk","cpp","bin"), values=c("black", "blue", "red", "grey"),
+  										labels=c(expression('P'["E"]*"(X)"),
+  														 expression('P'["CP"]^"K"*"(X)"),
+  														 expression('P'["CP"]^"P"*"(X)"),
+  														 expression('P'["Bin"]*"(X)")))+
+  scale_shape_manual(name="Models",
+  	 limits=c("emp","cpk","cpp","bin"), values=c(20,19,17,3),
+  										labels=c(expression('P'["E"]*"(X)"),
+  														 expression('P'["CP"]^"K"*"(X)"),
+  														 expression('P'["CP"]^"P"*"(X)"),
+  														 expression('P'["Bin"]*"(X)")))+
+  geom_errorbar(aes(ymin=q25,ymax=q75),width=.1)+ theme_bw()+
+  theme(text=element_text(size=20),
+  			legend.position=c(0.7,.7),
+  			legend.text=element_text(size=18),
+  			legend.key.size=unit(.4,"inches"))
+
+print(plp)
+dev.off()
 r=3
-write(
-paste(paste(alpha,seqlen, 
-signif(sum(abs(df2$prob-df2$cpnew)),digits=r),
-signif(sum(abs(df2$prob-df2$cppape)), digits=r),
-signif(sum(abs(df2$prob-df2$bin)),digits=r), sep=" & "), "\\\\"),
-file=sprintf("%s/%s_10-%d_m%d_tv.tabpart", figdir,pwmname,lalpha,markov))
 
 write(
-paste(paste(alpha,seqlen, 
-signif(sum(abs(df2$prob[qsig]-df2$cpnew[qsig])),digits=r),
-signif(sum(abs(df2$prob[qsig]-df2$cppape[qsig])),digits=r),
-signif(sum(abs(df2$prob[qsig]-df2$bin[qsig])),digits=r),sep=" & "), "\\\\"),
-file=sprintf("%s/%s_10-%d_m%d_5p.tabpart", figdir,pwmname,lalpha,markov))
+paste(
+markov, sprintf("$10^%d$",lalpha),
+signif(sum(abs(pe$prob-pk$prob)),digits=r),
+signif(sum(abs(pe$prob-pp$prob)),digits=r),
+signif(sum(abs(pe$prob-pb$prob)),digits=r),
+signif(sum(abs(pe$prob[qsig]-pk$prob[qsig])),digits=r),
+signif(sum(abs(pe$prob[qsig]-pp$prob[qsig])),digits=r),
+signif(sum(abs(pe$prob[qsig]-pb$prob[qsig])),digits=r),sep=" & "),
+file=sprintf("%s/../tab_chapter2/%s_10-%d_m%d.tabpart", figdir,pwmname,lalpha,markov))
 
 average=function(x) { return(seq(0,length(x)-1)%*% x)}
 variance=function(x) { return(sum(x*(seq(0,length(x)-1)-average(x))^2))}
 
-r=3
-write(
-paste(paste(alpha,seqlen, 
-signif(average(df2$prob),digits=r),
-signif(average(df2$cpnew),digits=r),
-signif(average(df2$cppape),digits=r),
-signif(average(df2$bin),digits=r),
-signif(variance(df2$prob),digits=r),
-signif(variance(df2$cpnew),digits=r),
-signif(variance(df2$cppape),digits=r),
-signif(variance(df2$bin),digits=r) ,sep=" & "), "\\\\"),
-file=sprintf("%s/%s_10-%d_m%d.mu_sig.tabpart", figdir,pwmname,lalpha,markov))
-
-
-#x=0:maxhits;
-#postscript(file=sprintf("%s/%s_10-%d_m%d.eps", figdir,pwmname,lalpha,markov))
-#plot(x,simc[[1]][x+1], main="",
-# xlab="# hits", ylab="Prob(hits)")
-
-#compound poisson distribution approximation
-#points(x,cpdist[[1]][x+1],col="green", type="p")
-#points(x,cppape[[1]][x+1],col="red", type="p")
-#points(dpois(0:maxhits, 2*(seqlen-nrow(pwm)+1)*numofseqs, alpha),col="gray")
-#points(x,pdist[[1]][x+1], col="red",type="l")
-#points(x,dbinom(x, 2*(seqlen-nrow(pwm)+1)*numofseqs, op$alpha),col="gray",type="o")
-
-#print(alpha)
-
-#legend(37,.1,c("simulated","Comp. Poisson (new)", "Comp. Poisson (Pape)", "Binomial"),
-# fill=c("black","green","red","gray"), cex=1.7)
-
-#plot(cumsum(simc[[1]]),cumsum(dist[[1]]))
-#plot(cumsum(simc[[1]]),cumsum(pape[[1]]))
-#dev.off()
