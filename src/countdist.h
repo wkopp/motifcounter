1067bedfa571b35b4067a676408a7cb5b0feada6
wkopp <kopp@molgen.mpg.de>
2016-11-16 16:58:02 +0100

    removed countdist

--- src/countdist.h
+++
@@ -1,7 +0,0 @@
-#ifndef countdist_h
-#define countdist_h
-
-void deleteCountDistribution(double *cp);
-double* initCountDistribution(int maxhit);
-void writeCountDistribution(FILE *f, double *cp, int maxhit);
-#endif
